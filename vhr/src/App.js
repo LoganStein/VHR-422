9519423e039760b83752f12529a08121a1b5eb47
Logan Stein <ljstein12@gmail.com>
2023-04-18 10:33:38 -0500

    Add pages and routing

--- vhr/src/App.js
+++ vhr/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
+import SearchResults from "./Pages/SearchResults";
+import Checkout from "./Pages/Checkout";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/Search" element={<SearchResults />} />
+        <Route path="/Listing" element={<Listing />} />
+        <Route path="/Checkout" element={<Checkout />} />
       </Routes>
     </Router>
   );
