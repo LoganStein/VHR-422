d32c4920fdc4bcc7cacce191fd7dc04371dcea5c
Logan Stein <ljstein12@gmail.com>
2023-04-23 19:36:44 -0500

    Style Sign in

--- vhr/src/Components/SignIn.jsx
+++ vhr/src/Components/SignIn.jsx
@@ -1,22 +1,44 @@
 import React from "react";
+import "../CSS/signIn.css";
 
 function SignIn(props) {
   return (
     <div className="signIn__container">
+      <span
+        className="signIn__exit"
+        onClick={() => {
+          props.setShowSignIn(false);
+        }}
+      >
+        X
+      </span>
       <div className="signIn__header-container">
         <span className="signIn__title">Sign In</span>
-        <span>For faster more convient checkout</span>
-        <span>
+        <span className="signIn__subtitle">
+          For faster more convient checkout
+        </span>
+        <span className="textLight">
           Don't have an account? <a href="#">Sign Up</a>
         </span>
       </div>
       <div className="signIn__form-container">
-        <label htmlFor="email">Email</label>
-        <input name="email" type="text" />
-        <label htmlFor="password">Password</label>
-        <input name="password" type="password" />
-        <input type="checkbox" name="remember" />
-        <a href="#">Forgot your password?</a>
+        <div className="label-input">
+          <label htmlFor="email">Email</label>
+          <input name="email" type="text" />
+        </div>
+        <div className="label-input">
+          <label htmlFor="password">Password</label>
+          <input name="password" type="password" />
+        </div>
+        <div className="signIn__remember-pass">
+          <div className="signIn__remember-pair">
+            <input type="checkbox" name="remember" />
+            <label id="remember" forhtml="remeber">
+              Remember Me
+            </label>
+          </div>
+          <a href="#">Forgot your password?</a>
+        </div>
         <button
           onClick={() => {
             props.setShowSignIn(false);
